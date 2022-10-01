85f4d1832fbe654dcd1b1d041e1d4ea68d2b61e1
钟承峰 <@DESKTOP-Q7K7CS1>
2022-10-01 09:57:17 +0800

    添加项目文件。

---
+++ TestSys/CustomComboBox.cpp
@@ -0,0 +1,85 @@
+﻿// CustomComboBox.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "TestSys.h"
+#include "CustomComboBox.h"
+#include <TlHelp32.h>
+
+// CustomComboBox
+
+IMPLEMENT_DYNAMIC(CustomComboBox, CComboBox)
+
+CustomComboBox::CustomComboBox()
+{
+}
+
+void CustomComboBox::SetProcessId(PULONG64 pPid)
+{
+	m_pPid = pPid;
+}
+
+CustomComboBox::~CustomComboBox()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CustomComboBox, CComboBox)
+	ON_CONTROL_REFLECT(CBN_SELCHANGE, &CustomComboBox::OnCbnSelchange)
+END_MESSAGE_MAP()
+
+
+
+// CustomComboBox 消息处理程序
+
+
+void CustomComboBox::OnCbnSelchange()
+{
+	// TODO: 在此添加控件通知处理程序代码
+    CString sName;
+    GetLBText(GetCurSel(), sName);
+    *m_pPid = GetPidFromProcessName(sName);
+    if (!*m_pPid)
+		MessageBox(L"PID获取失败！！！", _T("提示："));
+}
+
+void CustomComboBox::GetProcessInfo()
+{
+    //此结构描述了列表中的条目，该条目枚举了拍摄快照时驻留在系统地址空间中的进程。
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    //创建系统进程快照
+    HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    //获取第一个进程信息
+    Process32First(hProcessSnap, &pe32);
+
+    //创建用于接收系统进程的对象
+    CString sProcessNames;
+
+    //遍历进程名
+    do
+    {
+
+        //从进程快照中将进程名称传入接收的对象中
+        sProcessNames = pe32.szExeFile;
+
+        //将系统进程PID，名称存放入ProcessName中
+        m_ProcessNames[pe32.th32ProcessID] = sProcessNames;
+
+        AddString(sProcessNames);
+
+    } while (Process32Next(hProcessSnap, &pe32));//获取下一个进程信息
+
+    CloseHandle(hProcessSnap);
+}
+
+ULONG64 CustomComboBox::GetPidFromProcessName(CString sName)
+{
+    for (auto it = m_ProcessNames.begin(); it != m_ProcessNames.end(); it++) {
+        if (it->second == sName)
+            return it->first;
+    }
+    return 0;
+}
---
+++ TestSys/TestSysDlg.h
@@ -0,0 +1,65 @@
+﻿
+// TestSysDlg.h: 头文件
+//
+
+#pragma once
+#include "CustomComboBox.h"
+#include "CustomListCtrl.h"
+
+// CTestSysDlg 对话框
+class CTestSysDlg : public CDialogEx
+{
+// 构造
+public:
+	CTestSysDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TESTSYS_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+private:
+	ULONG64 m_pid = NULL;
+	CustomComboBox SysComboBox;
+	CustomListCtrl m_ListCtrl;
+	HANDLE m_pHandle = NULL;
+protected:
+	void InitCtrl();
+
+public:
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void CatMemoryData();
+	afx_msg ULONG64 getAddress();
+protected:
+	CString m_pAddr;
+	CString m_pOffSet1;
+	CString m_pOffSet2;
+	CString m_pOffSet3;
+	CString m_pOffSet4;
+	CString m_pOffSet5;
+
+public:
+	afx_msg void OnClose();
+	afx_msg void OnDestroy();
+	virtual BOOL DestroyWindow();
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+	virtual LRESULT WindowProc(UINT message, WPARAM wParam, LPARAM lParam);
+	afx_msg void OnIdok();
+	void PrintForReadMemory();
+	
+	afx_msg void OnBnClickedRefresh();
+};
